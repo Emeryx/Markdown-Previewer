8af5d7a3808ba3aee0bc23cabc4a1018bbec7206
Emeryx <omergal2003@gmail.com>
2024-04-02 17:28:29 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './App.sass';
+import {marked} from 'marked'
 
 function App() {
+
+  const [value, setValue] = useState<string>('<h1>Hi! Welcome to the project</h1>\n\n<h2>I am a cute sub heading</h2>\n\n>**Wowzers!**');
+  const [preview, setPreview] = useState<string>('');
+
+  const handleChange = (e : React.ChangeEvent<HTMLTextAreaElement> ) => {
+    setValue(e.target.value);
+  }
+
+  useEffect(() => {
+    console.log("Value changed");
+    const markdown = async () => {
+      try{
+        const result = await marked(value);
+        setPreview(result);
+      }
+      catch (err){
+        console.error(err);
+      }
+    }
+    markdown();
+  } , [value])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea id='editor' value={value} onChange={handleChange}></textarea>
+      <div id='preview' dangerouslySetInnerHTML={{ __html : preview }}></div>
     </div>
   );
 }
